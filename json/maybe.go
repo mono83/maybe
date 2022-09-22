4490c02eeac8345d9e99415cab3c511a6985c4d6
gotterdemarung <gotterdemarung@gmail.com>
2022-09-22 21:11:56 +0300

    :bulb: Added comments

--- json/maybe.go
+++ json/maybe.go
@@ -13,10 +13,13 @@ func Wrap[T any](m maybe.Maybe[T]) Maybe[T] {
 	return Maybe[T]{Maybe: m}
 }
 
+// Maybe in json package is a wrapper over standard maybe.Maybe
+// providing JSON serialization support
 type Maybe[T any] struct {
 	maybe.Maybe[T]
 }
 
+// MarshalJSON returns JSON bytes representation of Maybe contents
 func (m Maybe[T]) MarshalJSON() ([]byte, error) {
 	if m.IsEmpty() {
 		return jsonNull, nil
@@ -24,8 +27,9 @@ func (m Maybe[T]) MarshalJSON() ([]byte, error) {
 	return json.Marshal(m.Value())
 }
 
+// UnmarshalJSON fills Maybe container with decoded JSON data
 func (m *Maybe[T]) UnmarshalJSON(data []byte) error {
-	if bytes.Compare(data, jsonNull) == 0 {
+	if bytes.Equal(data, jsonNull) {
 		m.Maybe = maybe.Nothing[T]()
 	} else {
 		value := m.Value()
